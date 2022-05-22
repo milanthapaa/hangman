ea9890a74b4864182c57763a1790ea104d869773
Milan Thapa <45590593+milanthapaa@users.noreply.github.com>
2022-05-22 14:31:13 +0545

    added dict with visual value

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import string
 
 import random_words
+from visual import lives_dict
 
 # Welcome console message
 print("WELCOME TO THE HANGMAN GAME")
@@ -13,9 +14,9 @@ def hangman():
     word_letters = set(word)  # letter in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what user guessed
-
+    chance = 6
     # getting a user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and chance >= 0:
         # letter used
         print('You have used these letters: ', ''.join(used_letters))
 
@@ -27,12 +28,14 @@ def hangman():
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
+            print(lives_dict.get(chance))
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
         elif user_letter in used_letters:
             print("You've already used this character!")
         else:
             print("Invalid character. Please try again.")
+        chance -= 1
 
 
 hangman()
